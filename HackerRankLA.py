f0a8fe55f2285b14ad764d88bdbe01017737afdc
Wesley Neill <wesley.neill@gmail.com>
2020-02-23 11:18:36 -0800

    eigenvals exercises

--- HackerRankLA.py
+++ HackerRankLA.py
@@ -25,23 +25,60 @@ import numpy as np
 
 
 # Find the solution [x, y] for the matrix equation xA + yI = -A^2, where x and y are scalars
-A = np.array([[1,1,0], # 3x3 matrix
-              [0,1,0],
-              [0,0,1]])
+# A = np.array([[1,1,0], # 3x3 matrix
+#               [0,1,0],
+#               [0,0,1]])
+#
+# I = np.identity(3) # 3x3 matrix
+#
+# # Find solution of overdetermined system by least squares method
+# B = np.array([A.flatten(), I.flatten()]).transpose()  # 9x2 matrix
+# # z is the solution vector with dims 2x1
+# print(B)
+#
+# c = -1*np.linalg.matrix_power(A, 2).flatten().transpose() # 9x1 matrix
+# print(c)
+#
+# z = np.linalg.lstsq(B, c, rcond=-1)[0]
+# z = np.array(z)
+# print(z)
 
-I = np.identity(3) # 3x3 matrix
+# # solve A^1000
+# A = np.array([[-2, -9],
+#               [1, 4]])
 
-# Find solution of overdetermined system by least squares method
-B = np.array([A.flatten(), I.flatten()]).transpose()  # 9x2 matrix
-# z is the solution vector with dims 2x1
-print(B)
+# print(np.linalg.matrix_power(A, 1000))
 
-c = -1*np.linalg.matrix_power(A, 2).flatten().transpose() # 9x1 matrix
-print(c)
+# # Find the eigenvalues
+# A = np.array([[0,1],
+#               [-2, -3]])
+#
+# print(np.linalg.eigvals(A))
 
-z = np.linalg.lstsq(B, c, rcond=-1)[0]
-z = np.array(z)
-print(z)
+# # Find the determinant of a matrix
+# A = np.array([[3,0,0,-2,4],
+#               [0,2,0,0,0],
+#               [0,-1,0,5,-3],
+#               [-4,0,1,0,6],
+#               [0,-1,0,3,2]])
+
+# print(np.linalg.det(A))
+
+# Diagonalize A:
+
+A = np.array([[-2, -9],
+              [1, 4]])
+
+# step 1. Is it possible? For an nxn matrix, there must be n unique eigenvalues:
+vals, vects = np.linalg.eig(A)
+vals2 = np.linalg.eigvals(A)
+
+#Show 5 distinct eigenvalues
+print("Eigenvalues: ", vals)
+print("linalg.eigvals: ", vals2)
+
+print("Eigenvectors: ", vects)
+
 
 
 
